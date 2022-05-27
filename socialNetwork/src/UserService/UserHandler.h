943cb377b1791992d0d14bed2d53d372290b879f
Jack Pearson <jack@pearson.onl>
2022-05-27 09:51:49 -0700

    make RegisterUser use RegisterUserWithId

--- socialNetwork/src/UserService/UserHandler.h
+++ socialNetwork/src/UserService/UserHandler.h
@@ -283,102 +283,9 @@ void UserHandler::RegisterUser(
   ;
   LOG(debug) << "The user_id of the request " << req_id << " is " << user_id;
 
-  // Store user info into mongodb
-  mongoc_client_t *mongodb_client =
-      mongoc_client_pool_pop(_mongodb_client_pool);
-  if (!mongodb_client) {
-    ServiceException se;
-    se.errorCode = ErrorCode::SE_MONGODB_ERROR;
-    se.message = "Failed to pop a client from MongoDB pool";
-    throw se;
-  }
-  auto collection =
-      mongoc_client_get_collection(mongodb_client, "user", "user");
-
-  // Check if the username has existed in the database
-  bson_t *query = bson_new();
-  BSON_APPEND_UTF8(query, "username", username.c_str());
-  mongoc_cursor_t *cursor =
-      mongoc_collection_find_with_opts(collection, query, nullptr, nullptr);
-  const bson_t *doc;
-  bson_error_t error;
-  bool found = mongoc_cursor_next(cursor, &doc);
-  if (mongoc_cursor_error(cursor, &error)) {
-    LOG(error) << error.message;
-    bson_destroy(query);
-    mongoc_cursor_destroy(cursor);
-    mongoc_collection_destroy(collection);
-    mongoc_client_pool_push(_mongodb_client_pool, mongodb_client);
-    ServiceException se;
-    se.errorCode = ErrorCode::SE_MONGODB_ERROR;
-    se.message = error.message;
-    throw se;
-  } else if (found) {
-    LOG(warning) << "User " << username << " already existed.";
-    ServiceException se;
-    se.errorCode = ErrorCode::SE_THRIFT_HANDLER_ERROR;
-    se.message = "User " + username + " already existed";
-    bson_destroy(query);
-    mongoc_cursor_destroy(cursor);
-    mongoc_collection_destroy(collection);
-    mongoc_client_pool_push(_mongodb_client_pool, mongodb_client);
-    throw se;
-  } else {
-    bson_t *new_doc = bson_new();
-    BSON_APPEND_INT64(new_doc, "user_id", user_id);
-    BSON_APPEND_UTF8(new_doc, "first_name", first_name.c_str());
-    BSON_APPEND_UTF8(new_doc, "last_name", last_name.c_str());
-    BSON_APPEND_UTF8(new_doc, "username", username.c_str());
-    std::string salt = GenRandomString(32);
-    BSON_APPEND_UTF8(new_doc, "salt", salt.c_str());
-    std::string password_hashed = picosha2::hash256_hex_string(password + salt);
-    BSON_APPEND_UTF8(new_doc, "password", password_hashed.c_str());
-
-    auto user_insert_span = opentracing::Tracer::Global()->StartSpan(
-        "user_mongo_insert_client", {opentracing::ChildOf(&span->context())});
-    if (!mongoc_collection_insert_one(collection, new_doc, nullptr, nullptr,
-                                      &error)) {
-      LOG(error) << "Failed to insert user " << username
-                 << " to MongoDB: " << error.message;
-      ServiceException se;
-      se.errorCode = ErrorCode::SE_THRIFT_HANDLER_ERROR;
-      se.message =
-          "Failed to insert user " + username + " to MongoDB: " + error.message;
-      bson_destroy(query);
-      mongoc_cursor_destroy(cursor);
-      mongoc_collection_destroy(collection);
-      mongoc_client_pool_push(_mongodb_client_pool, mongodb_client);
-      throw se;
-    } else {
-      LOG(debug) << "User: " << username << " registered";
-    }
-    user_insert_span->Finish();
-    bson_destroy(new_doc);
-  }
-  bson_destroy(query);
-  mongoc_cursor_destroy(cursor);
-  mongoc_collection_destroy(collection);
-  mongoc_client_pool_push(_mongodb_client_pool, mongodb_client);
-
-  if (!found) {
-    auto social_graph_client_wrapper = _social_graph_client_pool->Pop();
-    if (!social_graph_client_wrapper) {
-      ServiceException se;
-      se.errorCode = ErrorCode::SE_THRIFT_CONN_ERROR;
-      se.message = "Failed to connect to social-graph-service";
-      throw se;
-    }
-    auto social_graph_client = social_graph_client_wrapper->GetClient();
-    try {
-      social_graph_client->InsertUser(req_id, user_id, writer_text_map);
-    } catch (...) {
-      _social_graph_client_pool->Remove(social_graph_client_wrapper);
-      LOG(error) << "Failed to insert user to social-graph-service";
-      throw;
-    }
-
-    _social_graph_client_pool->Keepalive(social_graph_client_wrapper);
-  }
+  UserHandler::RegisterUserWithId();
+  UserHandler::RegisterUserWithId(req_id, first_name, last_name, username,
+      password, user_id, carrier);
 
   span->Finish();
 }
@@ -604,7 +511,7 @@ void UserHandler::Login(std::string &_return, int64_t req_id,
   json login_json;
 
   if (login_mmc) {
-    // If not cached in memcached
+    // If cached in memcached
     LOG(debug) << "Found username: " << username << " in Memcached";
     login_json = json::parse(std::string(login_mmc, login_size));
     password_stored = login_json["password"];
